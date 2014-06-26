25af129d7195578514c479dad783dce690c272c1
Austin Meyer <austin.g.meyer@gmail.com>
2014-06-26 17:26:03 -0500

    Adding counts script.

---
+++ PPI_Setup/add_counts.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import foldx, re, shutil, random, os, math, sys, subprocess, glob
+import numpy as np
+from Bio import *
+import Bio.PDB as PDB
+
+def main():
+  output = open('add_counts.txt', 'w')
+  to_file = 'mutant\tcount\tbinding\tstab1\tstab2\n'
+  output.write(to_file)
+  output.close()
+
+  all_tried_A = 0
+  all_tried_C = 0
+  all_tried = {}
+
+  #All of the tried mutants
+  input_handle = open('all_mutants_tried.txt', 'r')
+  for a_line in input_handle.readlines():
+    split = a_line.split('\t')
+    if split[0] == 'count':
+      continue
+    else:
+      if float((split[1])[1:-5]) <= 156:
+        all_tried_A += 1
+        if rmWS(split[1]) not in all_tried.keys():
+          all_tried[rmWS(split[1])] = all_tried_A
+      else:
+        all_tried_C += 1
+        if rmWS(split[1]) not in all_tried.keys():
+          all_tried[rmWS(split[1])] = all_tried_C
+
+  #All of the kept mutants
+  input_handle = open('data.txt', 'r')
+  output = open('add_counts.txt', 'a')
+  for a_line in input_handle.readlines():
+    split = a_line.split('\t')
+    if split[0] == 'mutant':
+      continue
+    else:
+      chain = ""
+      if float((split[0])[1:-5]) <= 156:
+        chain = '_A'
+      else:
+        chain = '_C'
+
+      to_file = split[0] + chain +'\t' + str(all_tried[split[0]]) + '\t' + split[1] + '\t' + split[2] + '\t' + split[3] + '\n'
+      output.write(to_file)
+    
+  output.close()
+
+def rmWS(string):
+    return("".join(string.split()))
+
+#Run main program
+if __name__ == '__main__':
+   main()
