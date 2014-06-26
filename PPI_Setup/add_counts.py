d727b1d52ff322c34ced0c1287a9a80efb3d8279
Austin Meyer <austin.g.meyer@gmail.com>
2014-06-26 18:07:15 -0500

    Added relative and absolute counts to count adder.

--- PPI_Setup/add_counts.py
+++ PPI_Setup/add_counts.py
@@ -7,12 +7,13 @@ import Bio.PDB as PDB
 
 def main():
   output = open('add_counts.txt', 'w')
-  to_file = 'mutant\tcount\tbinding\tstab1\tstab2\n'
+  to_file = 'mutant\tabsolute_count\trelative_count\tbinding\tstab1\tstab2\n'
   output.write(to_file)
   output.close()
 
   all_tried_A = 0
   all_tried_C = 0
+  all_tried_both = 0
   all_tried = {}
 
   #All of the tried mutants
@@ -22,14 +23,16 @@ def main():
     if split[0] == 'count':
       continue
     else:
+      all_tried_both += 1
+
       if float((split[1])[1:-5]) <= 156:
         all_tried_A += 1
         if rmWS(split[1]) not in all_tried.keys():
-          all_tried[rmWS(split[1])] = all_tried_A
+          all_tried[rmWS(split[1])] = [all_tried_A, all_tried_both]
       else:
         all_tried_C += 1
         if rmWS(split[1]) not in all_tried.keys():
-          all_tried[rmWS(split[1])] = all_tried_C
+          all_tried[rmWS(split[1])] = [all_tried_C, all_tried_both]
 
   #All of the kept mutants
   input_handle = open('data.txt', 'r')
@@ -45,7 +48,7 @@ def main():
       else:
         chain = '_C'
 
-      to_file = split[0] + chain +'\t' + str(all_tried[split[0]]) + '\t' + split[1] + '\t' + split[2] + '\t' + split[3] + '\n'
+      to_file = split[0] + chain +'\t' + str(all_tried[split[0]][1]) + '\t' + str(all_tried[split[0][0]) + '\t' + split[1] + '\t' + split[2] + '\t' + split[3] + '\n'
       output.write(to_file)
     
   output.close()
