f68bae18e829eedc0e6526830946c52f9bba8dba
Austin Meyer <austin.g.meyer@gmail.com>
2014-06-26 18:40:58 -0500

    Removing Mac folder.

--- PPI_Setup_Mac/final_analysis.R
+++
@@ -1,9 +0,0 @@
-require(stringr)
-
-dat <- read.table('ancestral_comparisons.txt', sep='\t', header=T)
-
-as <- str_sub(levels(dat$name), start=-1) == "A"
-cs <- str_sub(levels(dat$name), start=-1) == "C"
-
-print(cor.test(dat[as,]$count, dat[as,]$interaction))
-print(cor.test(dat[cs,]$count, dat[cs,]$interaction))
