baefec73ff6e9851d23dff69632593f0b56f7928
Austin Meyer <austin.g.meyer@gmail.com>
2014-06-30 11:53:57 -0500

    Updated analysis R scripts.

--- PPI_Setup/final_analysis.R
+++ PPI_Setup/final_analysis.R
@@ -1,11 +1,23 @@
 last.letter <- function(this.string) {tmp.length <- nchar(this.string); substring(this.string, tmp.length, tmp.length)}
 
-dat <- read.table('ancestral_comparisons.txt', sep='\t', header=T, , stringsAsFactors=F)
+start <- 'both/'
+dirs <- list.files(start)
 
-final.letters <- sapply(dat$name, last.letter)
+ab.counts <- c()
+stab <- c()
+binding <- c()
+name <- c()
+chain <- c()
 
-as <- final.letters == "A"
-cs <- final.letters == "C"
+for(i in dirs) {
+  dat<-read.table(paste(start, i, "/ancestral_comparisons.txt", sep=''), sep='\t', header=T, stringsAsFactors=F);
+  final.letters <- sapply(dat$name, last.letter)
 
-print(cor.test(dat[as,]$count, dat[as,]$interaction))
-print(cor.test(dat[cs,]$count, dat[cs,]$interaction))
+  ab.counts <- append(ab.counts, dat$count);
+  stab <- append(stab, dat$stability);
+  binding <- append(binding, dat$interaction);
+  name <- append(name, dat$name)
+  chain <- append(chain, final.letters)
+}
+
+all.data <- data.frame(names=name, ab.counts=ab.counts, binding=binding, stab=stab, chain=chain)
