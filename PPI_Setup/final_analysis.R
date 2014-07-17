827333d26311dd1f9059a697687e1c46ffac8ed7
ausmeyer <austin.g.meyer@gmail.com>
2014-07-17 11:55:06 -0500

    Fixing the final_analysis.R script to work with the current output.

--- PPI_Setup/final_analysis.R
+++ PPI_Setup/final_analysis.R
@@ -1,23 +1,25 @@
 last.letter <- function(this.string) {tmp.length <- nchar(this.string); substring(this.string, tmp.length, tmp.length)}
 
-start <- 'both/'
+start <- 'B3_ancest_data/'
 dirs <- list.files(start)
 
-ab.counts <- c()
+ab.count <- c()
 stab <- c()
-binding <- c()
+ev.binding <- c()
+an.binding <- c()
 name <- c()
 chain <- c()
 
 for(i in dirs) {
-  dat<-read.table(paste(start, i, "/ancestral_comparisons.txt", sep=''), sep='\t', header=T, stringsAsFactors=F);
+  dat<-read.table(paste(start, i, sep=''), sep='\t', header=T, stringsAsFactors=F);
   final.letters <- sapply(dat$name, last.letter)
 
-  ab.counts <- append(ab.counts, dat$count);
+  ab.count <- append(ab.count, dat$count);
   stab <- append(stab, dat$stability);
-  binding <- append(binding, dat$interaction);
+  ev.binding <- append(ev.binding, dat$evolved_interaction);
+  an.binding <- append(an.binding, dat$ancestral_interaction);
   name <- append(name, dat$name)
   chain <- append(chain, final.letters)
 }
 
-all.data <- data.frame(names=name, ab.counts=ab.counts, binding=binding, stab=stab, chain=chain)
+all.data.B3 <- data.frame(names=name, ab.count=ab.count, ev.binding=ev.binding, an.binding=an.binding, stab=stab, chain=chain)
