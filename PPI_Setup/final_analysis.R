e5308497491793fa11a0389c31d3c1cc85f9720c
ausmeyer <austin.g.meyer@gmail.com>
2014-07-24 14:35:17 -0500

    Added survival analysis code to the final_analysis script.

--- PPI_Setup/final_analysis.R
+++ PPI_Setup/final_analysis.R
@@ -1,6 +1,8 @@
+rm(list = ls())
+
 last.letter <- function(this.string) {tmp.length <- nchar(this.string); substring(this.string, tmp.length, tmp.length)}
 
-start <- 'B2_ancest_data/'
+start <- 'B4_ancest_data/'
 dirs <- list.files(start)
 
 ab.count <- c()
@@ -10,7 +12,8 @@ an.binding <- c()
 name <- c()
 chain <- c()
 replicate <- c()
-
+survival <- c()
+survival.replicate <- c()
 count <- 0
 
 for(i in dirs) {
@@ -24,7 +27,12 @@ for(i in dirs) {
   an.binding <- append(an.binding, dat$ancestral_interaction);
   name <- append(name, dat$name)
   chain <- append(chain, final.letters)
-  replicate <- append(replicate, rep(factor(count), length(final.letters)))
+  replicate <- append(replicate, rep(as.character(count), length(final.letters)))
+  survival.replicate <- append(survival.replicate, rep(count, 1000))
+  for(i in 1:1000){ if(i > min(which(dat$ancestral_interaction > 0))) survival <- append(survival, 1) else survival <- append(survival, 0)}
+
 }
 
-all.data.B2 <- data.frame(names=name, ab.count=ab.count, ev.binding=ev.binding, an.binding=an.binding, stab=stab, chain=chain, replicate=replicate, set=rep('HS', length(replicate)))
+all.data.B4 <- data.frame(names=name, ab.count=ab.count, ev.binding=ev.binding, an.binding=an.binding, stab=stab, chain=chain, replicate=replicate, set=rep('LS', length(replicate)))
+
+survival.data.B4 <- data.frame(x=1:1000, y=rowMeans(as.data.frame(split(survival, survival.replicate))))
