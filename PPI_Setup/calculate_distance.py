5f78e61b6568302f1ab88870ae7e78568912df5d
Austin Meyer <austin.g.meyer@gmail.com>
2014-08-11 17:22:09 -0500

    Finished the calculate distance python script.

--- PPI_Setup/calculate_distance.py
+++ PPI_Setup/calculate_distance.py
@@ -3,24 +3,42 @@ from Bio.PDB import *
 import numpy as np
 
 def main(): 
-    if len( sys.argv ) != 4:
+    if len( sys.argv ) != 5:
         print '''
 
         You don't have the right number of arguments.
 
-        '''
-    elif len( sys.argv ) ==  4:
-        pdb = str(sys.argv[1])
-        chain = str(sys.argv[2])
-        distance_cutoff = float(sys.argv[3])
+        Input line should resemble:
+        python calculate_distance.py pdb_file_name chain distance_cutoff fixed_mutation_file
 
-    p = PDBParser()
-    structure = p.get_structure(pdb[0:-4], pdb)
+        '''
+    elif len( sys.argv ) ==  5:
+        pdb             = str(sys.argv[1])
+        chain           = str(sys.argv[2])
+        distance_cutoff = float(sys.argv[3])
+        file_name       = str(sys.argv[4])
+
+        p = PDBParser(QUIET=True)
+        structure = p.get_structure(pdb[0:-4], pdb)
     
-    distances = calculate_ca_distance(structure, chain, distance_cutoff)
-    print(distances[distances[:, 1] < distance_cutoff][:, 0])
+        distances = calculate_ca_distance(structure, chain)
+        mutation_sites = find_mutation_sites(file_name)
+        interface_sites = distances[distances[:, 1] < distance_cutoff][:, 0]
+        
+        print(interface_sites)
+
+        print(calculate_fraction_interface(interface_sites, mutation_sites))
     
-def calculate_ca_distance(structure, test_chain, distance_cutoff):
+def calculate_ca_distance(structure, test_chain):
+    '''This function has two inputs.  
+       1.) A structure object from the biopython parser.
+       2.) The chain to measure distances FROM in String format.
+     
+       This function will return one output.
+       1.) A numpy array containing two element lists of the form [Integer, Float] with the site number and 
+           the distance to the closest atom in its bound partner.
+    '''
+
     distances = []
     for atom1 in structure.get_atoms():
         full_id = atom1.get_full_id()
@@ -33,9 +51,54 @@ def calculate_ca_distance(structure, test_chain, distance_cutoff):
                 if test_chain != ref_id[2]:
                     tmp_3d_distance = test_position - atom2.get_coord()
                     tmp_distances.append(np.sqrt(tmp_3d_distance[0]**2 + tmp_3d_distance[1]**2 + tmp_3d_distance[2]**2))
-            distances.append([full_id[3][1], np.array(tmp_distances).min()])
+            distances.append([int(full_id[3][1]), np.array(tmp_distances).min()])
             
     return(np.array(distances))
+
+def find_mutation_sites(file_name):
+    '''This function has one input.
+       1.) A file name in String format for the fixed mutation data from main.py
+           Traditionally this file is named final_data.txt, but 
+           could be anything the simulator used.
+
+       This function will return one output.
+       1.) A list of integers of the sites where mutations fixed
+    '''
+  
+    in_file = open(file_name, 'r')
+    
+    mutation_sites = []
+
+    for a_line in in_file.readlines():
+        split = a_line.split('\t')
+        if split[0] == 'file':
+            continue
+        else:
+            mutation_sites.append(int(split[1][1:-1]))
+
+    return(mutation_sites)
+
+def calculate_fraction_interface(interface_sites, mutation_sites):
+    '''This function has two inputs.
+       1.) A list of the interface sites in Integer format
+       2.) A list of mutation sites in Integer Format
+
+       This function has one output.
+       1.) A float that is the fraction of mutations in the interface
+    '''
+    
+    interface_count = 0
+    mutation_count  = 0
+
+    for mutation_site in mutation_sites:
+        if mutation_site in interface_sites:
+            interface_count += 1
+        mutation_count += 1
+
+    return(float(interface_count)/mutation_count)
         
+
 if __name__ == "__main__":
     main()
+
+
