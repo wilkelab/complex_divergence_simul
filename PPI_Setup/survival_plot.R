dea7a7acd27494ee92fc42958ebca9beff33890c
ausmeyer <austin.g.meyer@gmail.com>
2014-08-12 17:17:11 -0500

    Final survival analysis script.

--- PPI_Setup/survival_plot.R
+++ PPI_Setup/survival_plot.R
@@ -1,3 +1,5 @@
+require(survival)
+
 rm(list = ls())
 survival.value = -7
 this.chain = "A"
@@ -8,94 +10,78 @@ get.data <- function(this.folder, which.chain) {
   start <- this.folder
   dirs <- list.files(start)
   
-  ab.count <- c()
-  stab <- c()
-  ev.binding <- c()
-  an.binding <- c()
-  name <- c()
-  chain <- c()
-  replicate <- c()
-  survival <- c()
-  survival.replicate <- c()
-  count <- 0
+  survival.count <- c()
+  survival.divergence <- c()
+  status.divergence <- c()
+  status.count <- c()
   
   for(i in dirs) {
     dat <- read.table(paste(start, i, sep=''), sep='\t', header=T, stringsAsFactors=F);
     final.letters <- sapply(dat$name, last.letter)
     dat <- dat[final.letters == which.chain, ] 
-    final.letters <- final.letters[final.letters == which.chain]
     
-    count <- count + 1
+    cutoff.count <- min(dat$count[which(dat$ancestral_interaction > survival.value)])
+    cutoff.divergence <- 1 - min(dat$identity[which(dat$ancestral_interaction > survival.value)])
     
-    ab.count <- append(ab.count, dat$count);
-    stab <- append(stab, dat$stability);
-    ev.binding <- append(ev.binding, dat$evolved_interaction);
-    an.binding <- append(an.binding, dat$ancestral_interaction);
-    name <- append(name, dat$name)
-    chain <- append(chain, final.letters)
-    replicate <- append(replicate, rep(as.character(count), length(final.letters)))
-    survival.replicate <- append(survival.replicate, rep(count, 1000))
+    cutoff.count[is.infinite(cutoff.count) | is.na(cutoff.count)] <- max(dat$count)
+    cutoff.divergence[is.infinite(cutoff.divergence) | is.na(cutoff.divergence)] <- max(1 - dat$identity)
     
-    cutoff <- dat$count[min(which(dat$ancestral_interaction > survival.value))]
-    cutoff[is.na(cutoff)] <- 1000
+    survival.divergence <- append(survival.divergence, cutoff.divergence)
+    status.divergence <- append(status.divergence, as.numeric(!cutoff.divergence == max(1 - dat$identity)))
     
-    for(i in 1:1000){ if(i > cutoff) survival <- append(survival, 0) 
-                      else survival <- append(survival, 1)}
+    survival.count <- append(survival.count, cutoff.count)
+    status.count <- append(status.count, as.numeric(!cutoff.count == max(dat$count)))
   }
   
-  tmp.data <- data.frame(names=name, ab.count=ab.count, ev.binding=ev.binding, an.binding=an.binding, stab=stab, 
-                         chain=chain, replicate=replicate, set=rep('LS', length(replicate)))
-  
-  tmp.survival.data <- data.frame(x=1:1000, y=rowMeans(as.data.frame(split(survival, survival.replicate))))
+  tmp.survival.data <- data.frame(survival.count=survival.count,
+                                  survival.divergence=survival.divergence,
+                                  status.count=status.count, 
+                                  status.divergence=status.divergence
+  )
   
   return(tmp.survival.data)
-  
 }
 
-survival.data.B1 <- get.data('B1_ancest_data/', this.chain)
-survival.data.B2 <- get.data('B2_ancest_data/', this.chain)
-survival.data.B3 <- get.data('B3_ancest_data/', this.chain)
-survival.data.B4 <- get.data('B4_ancest_data/', this.chain)
-survival.data.UnB <- get.data('UnB_data/', this.chain)
+survival.data.WT <- get.data('~/Desktop/WT_data/', this.chain)
+survival.data.UnB <- get.data('~/Desktop/UnS_data/', this.chain)
 
-survival.data <- data.frame(x=rep(1:1000, 5), y=c(survival.data.B1$y, survival.data.B2$y, survival.data.B3$y, survival.data.B4$y, survival.data.UnB$y), 
-                            replicate=c(rep('b1',1000), rep('b2', 1000), rep('b3', 1000), rep('b4', 1000), rep('UnB', 1000)))
+survival.data <- data.frame(time=c(survival.data.WT$survival.count, survival.data.UnB$survival.count), 
+                            status=c(survival.data.WT$status.count, survival.data.UnB$status.count), 
+                            replicate=c(rep('WT', length(survival.data.WT$status.count)), rep('UnS', length(survival.data.UnB$status.count)))
+)
 
+#survival.data <- data.frame(time=c(survival.data.UnB$survival.count), 
+#                            status=c(survival.data.UnB$status.count), 
+#                            replicate=c(rep('UnB', length(survival.data.UnB$status.count)))
+#)
 
-survival.lines <- function(df) {
-  require(ggplot2)
-  require(grid)
-  
-  graphics.off()
-  the_pointsize=18
-  theme_set(theme_bw(base_size=the_pointsize))
-  old_theme <- theme_update(panel.border=element_blank(),
-                            axis.line=element_line(),
-                            panel.grid.minor=element_blank(),
-                            panel.grid.major=element_blank(),
-                            panel.background=element_blank(),
-                            panel.border=element_blank(),
-                            axis.line=element_line())
-  
-  g <- ggplot(df, aes(x=x, y=y, color=replicate)) + geom_line(size=2.5)
-  g <- g + theme(strip.background=element_blank())
-  g <- g + ylab('Ancestral Binding')
-  g <- g + xlab('Time (Mutations Attempted)')
-  g <- g + scale_x_continuous(breaks=seq(0, 1000, 250), limits=c(0, 1000))
-  #  g <- g + scale_y_continuous(breaks=seq(500, 1000, 100), limits=c(550, 900))
-  g <- g + theme(panel.border=element_blank(), axis.line=element_line())
-  g <- g + theme(axis.title.x = element_text(size=24, vjust=-1))
-  g <- g + theme(axis.text.x = element_text(size=24))
-  g <- g + theme(axis.title.y = element_text(size=24, vjust=2))
-  g <- g + theme(axis.text.y = element_text(size=24))
-  g <- g + theme(axis.line = element_line(colour = 'black', size = 1))
-  g <- g + theme(axis.ticks = element_line(colour = 'black', size = 1))
-  g <- g + theme(plot.margin=unit(c(1.5, 1.5, 1.5, 1.5), "lines"))
-  g <- g + theme(axis.ticks.margin = unit(0.25, "cm"))
-  g <- g + theme(legend.position = "none")
-  
-  ggsave(g, file=paste('~/Desktop/survival_', this.chain, '.pdf', sep=''), width=10, height=10)
-  return(g)
-}
+fit = survfit(Surv(time,status)~replicate, data=survival.data)
+print(survdiff(Surv(time,status)~replicate, data=survival.data))
 
-survival.lines(survival.data)
+pdf(paste('~/Desktop/survival_', this.chain, '.pdf', sep=''), height=11, width=12)
+par(mar=c(5,5,1,2)+0.1)
+par(mgp=c(3, 1, 0))
+par(family = 'Helvetica')
+
+plot(fit, xlab="Time (Mutations Attempted)", 
+     ylab="Survival Probability", 
+     col=c('red', 'blue'), 
+     cex.lab=1.75,
+     mark=19,
+     axes=F,
+     xlim=c(0,1000),
+     lwd=1.5
+)
+
+axis( 1, 
+      cex.axis=1.75,
+      at = seq(0, 1000, 100),
+      lwd=1.5)
+axis( 2, 
+      cex.axis=1.75,
+      #at = seq(0, 1, 0.1),
+      lwd=1.5)
+
+legend( 0.1, 0.1, c('UnB', 'WT'), col=c('red', 'blue'), lty=1, cex=1.4, lwd=2)
+
+dev.off()
